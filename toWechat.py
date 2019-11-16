292eac88437d186a199e2e8376150bc778b69588
zhuYe <loveqianool@users.noreply.github.com>
2019-11-17 02:08:55 +0800

    Update toWechat.py

--- toWechat.py
+++ toWechat.py
@@ -1,11 +1,9 @@
-cat > toWechat.py << EOF
 # encoding=utf8
 #!/usr/bin/python
-#pip install beautifulsoup4 requests
-
 import requests
 from bs4 import BeautifulSoup
 import time
+#pip install beautifulsoup4 requests
 
 pushurl='https://zhuye.heliohost.org/work.php?msg='
 headers = {'user-agent': 'Mozilla/5.0 (Android 9; Mobile; rv:68.0) Gecko/68.0 Firefox/68.0'}
@@ -16,8 +14,8 @@ def getposturl():
   r = s.get(url, headers=headers)
   soup = BeautifulSoup(r.text,'html.parser')
   pid = r.text[r.text.find('tid')+4:r.text.find('tid')+10]
-  post_url = "https://www.hostloc.com/thread-{}-1-1.html".format(pid)
-  return post_url
+  posturl = "https://www.hostloc.com/thread-{}-1-1.html".format(pid)
+  return posturl
 
 def getnewpost():
   r = s.get(posturl, headers=headers)
@@ -26,13 +24,18 @@ def getnewpost():
   content = soup.find('div',class_='message').text.strip()
   return title,content
 
-posturl = getposturl()
+newpost = getposturl()
 
-while posturl != getposturl():
-  posturl = getposturl()
-  firstArr = getnewpost()
-  title = firstArr[0]
-  content = firstArr[1]
-  finalUrl = pushurl + '&title=' + title + '&msg=' + content + '&url=' + posturl
-  s.get(finalUrl)
+while True:
   time.sleep(30)
+  posturl = getposturl()
+  if posturl != newpost:
+    newpost = posturl
+    firstArr = getnewpost()
+    title = firstArr[0]
+    content = firstArr[1]
+    finalUrl = pushurl + '&title=' + title + '&msg=' + content + '&url=' + posturl
+    s.get(finalUrl)
+    pass
+  else:
+    pass
