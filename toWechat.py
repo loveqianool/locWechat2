4a34f47c18a4e2ccff199c85ba01783193ef3d57
zhuYe <loveqianool@users.noreply.github.com>
2019-11-18 01:55:53 +0800

    Update toWechat.py

--- toWechat.py
+++ toWechat.py
@@ -6,19 +6,25 @@ import time
 #pip install beautifulsoup4 requests
 
 pushurl='https://zhuye.heliohost.org/work.php?msg='
+login = 'https://www.hostloc.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1'
+listurl = 'https://www.hostloc.com/forum.php?mod=forumdisplay&fid=45&filter=author&orderby=dateline'
+
 headers = {'user-agent': 'Mozilla/5.0 (Android 9; Mobile; rv:68.0) Gecko/68.0 Firefox/68.0'}
+data={"username":'wmw',"password":'Mjj123456'}
+
 s = requests.session()
+request = s.post(login, data = data,headers=headers)
+cookie = request.cookies
 
 def getposturl():
-  url = 'https://www.hostloc.com/forum.php?mod=forumdisplay&fid=45&filter=author&orderby=dateline'
-  r = s.get(url, headers=headers)
+  r = s.get(listurl, headers=headers)
   soup = BeautifulSoup(r.text,'html.parser')
   pid = r.text[r.text.find('tid')+4:r.text.find('tid')+10]
   posturl = "https://www.hostloc.com/thread-{}-1-1.html".format(pid)
   return posturl
 
 def getnewpost():
-  r = s.get(posturl, headers=headers)
+  r = s.get(posturl, headers=headers, cookies=cookie)
   soup = BeautifulSoup(r.text,'html.parser')
   title = soup.find(attrs={"name":"keywords"})['content']
   content = soup.find('div',class_='message').text.strip()
